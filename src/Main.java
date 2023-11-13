84b5e85e2fd1e4c1e8301c15d3ee089f91103a01
strifedeeno <coolarnay@gmail.com>
2023-11-13 08:53:35 -0800

    fixed typo

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,12 @@ public class Main {
     public static void main(String[] args) {
         int[] one ={8};
         int[] two ={14, 7};
-        int[] thre ={7, 1, 3, 2, 0, 4};
+        int[] three ={7, 1, 3, 2, 0, 4};
         int[] four={10, 8, 9, 5, 5};
         int[] give={12, 11, 10, 10, 8, 7};
         arrayMystery4(one);
         arrayMystery4(two);
-        arrayMystery4(thre);
+        arrayMystery4(three);
         arrayMystery4(four);
         arrayMystery4(give);
     }
