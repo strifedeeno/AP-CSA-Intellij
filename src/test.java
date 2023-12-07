8839e44163811afde3b76bf68abe4e906db59832
strifedeeno <coolarnay@gmail.com>
2023-12-07 10:15:52 -0800

    10:15 am dec 7

--- src/test.java
+++ src/test.java
@@ -2,51 +2,6 @@ import java.util.Arrays;
 
 public class test {
     public static void main(String[] args){
-        double val = -0.7;
-
-        int roundedVal = (int) (val + 0.5);
-
-        System.out.println(roundedVal);
-    }
-    public static int mystery(boolean a, boolean b, boolean c)
-
-    {
-
-        int answer = 7;
-
-
-
-        if (!a)
-
-        {
-
-            answer += 1;
-
-        }
-
-
-
-        if (b)
-
-        {
-
-            answer += 2;
-
-        }
-
-
-
-        if (c)
-
-        {
-
-            answer += 4;
-
-        }
-
-
-
-        return answer;
 
     }
 }
