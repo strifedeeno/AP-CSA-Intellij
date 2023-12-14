73504ad9b6f65e0d408495d62f9c5923e405f89e
strifedeeno <coolarnay@gmail.com>
2023-12-14 08:32:59 -0800

    dec 14 8:32

--- src/test.java
+++ src/test.java
@@ -4,10 +4,11 @@ public class test {
     public static void main(String[] args) {
         int x = 0, y = 3;
         String op = "/";
-        if (op.equals("/") && (x != 0) && (y/x > 2))
-        {System.out.println("“OK”");}
-else
-        {System.out.println("“FAILED”");}
+        if (op.equals("/") && (x != 0) && (y / x > 2)) {
+            System.out.println("“OK”");
+        } else {
+            System.out.println("“FAILED”");
+        }
     }
 
 }
