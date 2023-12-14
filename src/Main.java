df25511d83c8ea7a69e83cf817940d6fe462832c
strifedeeno <coolarnay@gmail.com>
2023-12-14 08:32:17 -0800

    dec 14 8:31

--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,6 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] one ={8};
-        int[] two ={14, 7};
-        int[] three ={7, 1, 3, 2, 0, 4};
-        int[] four={10, 8, 9, 5, 5};
-        int[] give={12, 11, 10, 10, 8, 7};
-        arrayMystery4(one);
-        arrayMystery4(two);
-        arrayMystery4(three);
-        arrayMystery4(four);
-        arrayMystery4(give);
-    }
-
-    public static void arrayMystery4(int[] nums) {
-        for (int i = 0; i < nums.length - 1; i++) {
-            if (nums[i] > nums[i + 1]) {
-                nums[i + 1]++;
-            }
-        }
-        System.out.println(Arrays.toString(nums));
+        System.out.println("Hi");
     }
 }
--- src/test.java
+++ src/test.java
@@ -1,8 +1,13 @@
 import java.util.Arrays;
 
 public class test {
-    public static void main(String[] args){
-
+    public static void main(String[] args) {
+        int x = 0, y = 3;
+        String op = "/";
+        if (op.equals("/") && (x != 0) && (y/x > 2))
+        {System.out.println("“OK”");}
+else
+        {System.out.println("“FAILED”");}
     }
 
 }
